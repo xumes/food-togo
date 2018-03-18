677cc1c1a55b6d3943d1ca618d073444e274f709
Reginaldo Santos <reginaldosantos.br@gmail.com>
2018-03-18 15:16:39 -0300

    Project refactory - including axios to make http requests

---
+++ src/API/API.js
@@ -0,0 +1,3 @@
+function newCustomer(customer) {
+    return    
+}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
-import logo from './images/logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import axios from 'axios'
+
 import './App.css';
-import Home from './Components/Home';
+import Home from './Components/Home'
+import CustomerNew from './Components/Customer/CustomerNew'
+import CustomerLogin from './Components/Customer/CustomerLogin'
+import Cousines from './Components/Product/Cousines'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cousines:[]
+    }
+
+    this.getCousines()
+  }
+
+  getCousines() {
+    const url = 'http://api-vanhack-event-sp.azurewebsites.net/api/v1/Cousine'
+    axios
+      .get(url)
+      .then(data => {
+        console.log("return from getcousines", data)
+        this.setState({ cousines: data })
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" exact render={() => <Home />} />
+          <Route path="/sign-up" exact render={() => <CustomerNew />} />
+          <Route path="/login" exact render={() => <CustomerLogin />} />
+          <Route path='/cousines' exact render={() => <Cousines cousines={this.state.cousines} />} />
+        </div>
+      </Router>
     );
   }
 }
